dbf93754c08ea56dbbaa263522a88e49d856a99b
Ben1152000 <benjamindarnell00@gmail.com>
2020-12-19 00:56:35 -0800

    Add sign-in/sign-out button

--- ottertab/src/App.js
+++ ottertab/src/App.js
@@ -3,14 +3,23 @@ import './App.css';
 import { Center } from './components/core.js';
 import { Auth } from './components/auth.js';
 import { Calendar } from './components/calendar.js';
+import { Layout, Row, Col } from 'antd';
 
+const { Header, Footer, Sider, Content } = Layout;
 
 function App() {
   return (
     <div className="App">
-        <Center/>
-        <Auth/>
-        <Calendar/>
+        <Header></Header>
+        <Content style = {{ height: 100 + '%' }}>
+          <Center/>
+          <Calendar/>
+        </Content>
+        <Footer style = {{ position: 'absolute', bottom: 0, width: 100 + '%' }}>
+          <div style={{float: 'left'}}>
+            <Auth justify='start'/>
+          </div>
+        </Footer>
     </div>
   );
 }
--- ottertab/src/components/auth.js
+++ ottertab/src/components/auth.js
@@ -1,18 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import { gapi, loadAuth2 } from 'gapi-script';
+import { Button } from 'antd';
+import { ReactComponent as Logo } from '../assets/images/google-logo.svg';
 
 const config = {
     clientId: process.env.REACT_APP_GAPI_CLIENT_ID,
     apiKey: process.env.REACT_APP_GAPI_API_KEY,
     scope: "https://www.googleapis.com/auth/calendar",
     discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"]
-  }
+}
 
 export const Auth = (props) => {
     const calendar = 'primary';
     
     const [signedIn, setSignedIn] = useState(false);
-
 
     const initClient = () => {
         window.gapi.client.init(config).then(() => {
@@ -33,16 +34,18 @@ export const Auth = (props) => {
         setSignedIn(window.gapi.auth2.getAuthInstance().isSignedIn.get());
     }
 
-
-   useEffect(() => {
+    useEffect(() => {
         const initAuth = async () => {
             await window.gapi.load('client:auth2', initClient);
         }
         initAuth();
-   }, []);
+    }, []);
 
-   return <React.Fragment>
-       <button onClick={(e) => handleAuthClick()}>Sign in</button>
-   </React.Fragment>
+    return <React.Fragment>
+        <Button type="primary" onClick={(e) => {signedIn? handleSignoutClick(): handleAuthClick()}}>
+            {/* <Logo width="1.3em" height="1.3em"></Logo> */}
+            {signedIn? "Sign Out": "Sign In"}
+        </Button>
+    </React.Fragment>
 
 }
