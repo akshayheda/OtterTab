bb195227e32e566b399489d748f7fba5d4604ed4
Ben1152000 <benjamindarnell00@gmail.com>
2020-12-19 18:16:25 -0800

    CSS Beautification

--- ottertab/src/App.js
+++ ottertab/src/App.js
@@ -1,7 +1,6 @@
 import './App.less';
 import { Center } from './components/core.js';
 import { Auth } from './components/auth.js';
-import { Calendar } from './components/calendar.js';
 import { Layout, Row, Col } from 'antd';
 import React, { useState, useEffect } from 'react';
 
@@ -12,18 +11,16 @@ function App() {
   const [gapiLoaded, setLoaded] = useState(false);
 
   return (
-    <div className="App">
-        <Header></Header>
-        <Content style = {{ height: 100 + '%' }}>
-          <Center/>
-          <Calendar loaded={gapiLoaded}/>
-        </Content>
-        <Footer style = {{ position: 'absolute', bottom: 0, width: 100 + '%' }}>
-          <div style={{float: 'left'}}>
-            <Auth justify='start' setLoaded={status => setLoaded(status)}/>
-          </div>
-        </Footer>
-    </div>
+    <Layout className="App" style = {{height: 100 + 'vh'}}>
+      <Content>
+        <Center loaded={gapiLoaded}/>
+      </Content>
+      <Footer style = {{ backgroundColor: 'rgba(255, 255, 255, 0.0)', position: 'absolute', bottom: 0, width: 100 + '%' }}>
+        <div style={{float: 'left'}}>
+          <Auth justify='start' setLoaded={status => setLoaded(status)}/>
+        </div>
+      </Footer>
+    </Layout>
   );
 }
 
--- ottertab/src/components/core.js
+++ ottertab/src/components/core.js
@@ -2,20 +2,22 @@ import React from 'react';
 import { Clock } from './clock.js';
 import { Day } from './day.js';
 import { SearchBar } from './search.js';
+import { Calendar } from './calendar.js';
 
 import {Row, Col} from 'antd';
 
-
-export const Center = () => {
-    return <div>
-            <Row justify='center'>
-                <Clock/> 
-            </Row>
-            <Row justify='center'>
-                <Day/>
-            </Row>
-            <Row justify='center' span={12}>
-                <SearchBar/>
-            </Row>
-        </div>;
+export const Center = (loaded) => {
+    return <Col justify='center' style={{position: 'relative', top: 5 + 'rem', margin: 'auto', padding: 1 + 'rem', width: 100 + '%', maxWidth: 50 + 'rem'}}>
+        <Row justify='center' style={{}}>
+            <Clock/> 
+        </Row>
+        <Row justify='center' style={{marginBottom: 2 + 'rem'}}>
+            <Day/>
+        </Row>
+        <Row justify='center' style={{marginBottom: 2 + 'rem'}} span={12}>
+            <SearchBar/>
+        </Row>
+        <Calendar loaded={loaded}/>
+        
+    </Col>
 }
